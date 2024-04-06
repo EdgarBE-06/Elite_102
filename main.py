99ff08be516981c3b56d2dc260979bb4a8bb3b03
Edgar <edgar29be06@gmail.com>
2024-04-06 17:23:13 -0500

    added the mysql connection and created a table

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import mysql.connector
+
 def welcome_message():
     name = input("Hey there sport what's your name?")
     print(f"Hi {name} how's your day been")
 
 welcome_message()
+
+connection = mysql.connector.connect(
+    user = 'root',
+    database = 'bank_account',
+    password = 'password'
+)
+
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM user_account")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
